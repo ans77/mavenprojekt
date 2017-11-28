fc3682a77988c6328110a50d5defcf9bfdd4806c
Jannik Ivosevic <example@example.com>
2017-11-28 13:54:02 +0100

    files hinzugefügt

---
+++ src/main/java/de/ans/mvnprogramm.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class mvnprogramm {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		String auswahl = scanner.next().toUpperCase();
+		System.out.println(auswahl);
+		scanner.close();
+		
+	}
+}
