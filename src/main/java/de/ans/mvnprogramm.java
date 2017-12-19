779750eed2773b4d26a77abdb34cf868b220c239
Jannik Ivosevic <example@example.com>
2017-12-19 13:56:53 +0100

    Änderungen durchgeführt.

--- src/main/java/de/ans/mvnprogramm.java
+++ src/main/java/de/ans/mvnprogramm.java
@@ -1,10 +1,17 @@
+package de.ans;
+
 import java.util.Scanner;
 
 
 public class mvnprogramm {
 
+
+	 /**
+    * Eine Funktion, die eine Eingabe einliesst und ausgibt.
+    */
 	public static void main(String[] args) {
 		
+		//Ein Programm, welches ein Text einließt und mit Großbuchstaben wieder ausgibt.
 		Scanner scanner = new Scanner(System.in);
 		String auswahl = scanner.next().toUpperCase();
 		System.out.println(auswahl);
